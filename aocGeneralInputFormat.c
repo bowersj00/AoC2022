c755cda095dc17bf220a2d5313062cb0f05d1804
Justin Bowers <justinbowers@ATM0560881DYN.dhcp.missouri.edu>
2022-12-14 23:30:24 -0600

    add skeleton code and remove extranneous a.out

---
+++ aocGeneralInputFormat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    int c1 = 0;
+    int c2 = 0;
+
+    fp = fopen("aoc2022DayXInput.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+
+    }
+
+    printf("Part 1: %d\nPart 2: %d\n", c1, c2);
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
